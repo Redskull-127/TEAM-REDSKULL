b5ea5ee732081bce2da3bb4a9427828eb022b075
Meer Tarbani <76103614+Redskull-127@users.noreply.github.com>
2022-06-13 07:09:54 +0000

    todo

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+import styles from "../styles/Header.module.css";
+
+export default function Headers() {
+    return(<>
+        <div className={styles.Header}>
+            <h1>Working on it!</h1>
+        </div>
+    </>);
+}
