d1829f879b63ab4ce6be2bc84f4eedc8024906fb
Meer Tarbani <76103614+Redskull-127@users.noreply.github.com>
2022-06-13 07:14:04 +0000

    h

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,11 @@
 import styles from "../styles/Header.module.css";
 
 export default function Headers() {
-    return(<>
-        <div className={styles.Header}>
-            <h1>Working on it!</h1>
-        </div>
-    </>);
+  return (
+    <>
+      <div className={styles.Header}>
+        <h1>Working on it!</h1>
+      </div>
+    </>
+  );
 }
