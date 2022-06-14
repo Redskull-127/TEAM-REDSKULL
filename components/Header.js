3a10e2472149c47ffe14428a1d864d8db708d1e9
Meer Tarbani <76103614+Redskull-127@users.noreply.github.com>
2022-06-14 15:02:35 +0530

    Never Ending

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,110 @@
 import styles from "../styles/Header.module.css";
+import Image from "next/image";
+import React, { useState } from "react";
+import { auth } from "../firebase/FirebaseConfig";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 export default function Headers() {
-  return (
-    <>
-      <div className={styles.Header}>
-        <h1>Structure Defined</h1>
+  const [isOpen, setIsOpen] = useState(false);
+  const [isemail, setEmail] = useState("");
+  const [ispassword, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLogedIn, setIsLoggedIn] = useState(false);
+
+  function log() {
+    setEmail(document.getElementById("exampleFormControlInput1").value);
+    setPassword(document.getElementById("exampleFormControlInput2").value);
+    signInWithEmailAndPassword(auth, isemail, ispassword)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setIsLoggedIn(true);
+        window.open("/", "_self");
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIsLoggedIn(false);
+      });
+  }
+  if (error) {
+    return (
+      <div>
+        <h1>{error}</h1>
       </div>
-    </>
-  );
+    );
+  } else {
+    return (
+      <>
+        <div className={styles.HeaderTop}>
+          <img src="/Assets/CY-Bock.png" />
+          <div
+            className={styles.divlogin}
+            onClick={(e) => {
+              setIsOpen(true);
+            }}
+          >
+            <a>Login</a>
+          </div>
+        </div>
+        {isOpen ? (
+          <center>
+            <div className={styles.logindiv} id="logindiv">
+              <div className={styles.upper}>
+                <i
+                  className="bi bi-arrow-left"
+                  onClick={(e) => {
+                    setIsOpen(false);
+                  }}
+                ></i>
+              </div>
+              <div className={styles.mid}>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleFormControlInput1"
+                    className="form-label"
+                  >
+                    Email address
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleFormControlInput1"
+                    placeholder="name@example.com"
+                  />
+                </div>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleFormControlInput2"
+                    className="form-label"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="exampleFormControlInput2"
+                    placeholder="********"
+                  />
+                </div>
+              </div>
+              <div className={styles.lower}>
+                <div
+                  className={styles.divlogin}
+                  onClick={(e) => {
+                    log();
+                  }}
+                >
+                  <a>Login</a>
+                </div>
+              </div>
+            </div>
+          </center>
+        ) : (
+          <div className={styles.details} id="details">
+            <h1>Some Details Here Later</h1>
+          </div>
+        )}
+      </>
+    );
+  }
 }
---
+++ firebase/FirebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+initializeApp({
+  apiKey: "AIzaSyCh8nd7w9QbWD9z-BL5-Z3-kV_HoNgR--E",
+  authDomain: "team-redskull.firebaseapp.com",
+  projectId: "team-redskull",
+  storageBucket: "team-redskull.appspot.com",
+  messagingSenderId: "727979282334",
+  appId: "1:727979282334:web:1e2bbfed7c0a44b269d08f",
+  measurementId: "G-C4T38F9X70",
+});
+
+const auth = getAuth();
+
+export {auth};
