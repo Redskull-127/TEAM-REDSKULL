64b596b161900ab5c19251d689f9c271e8914005
Meer Tarbani <76103614+Redskull-127@users.noreply.github.com>
2022-06-14 15:06:20 +0530

    Never Ending

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    loader: "akamai",
+    path: ""
+  },
+  basePath: "",
+  assetPrefix: "",
 }
 
 module.exports = nextConfig
